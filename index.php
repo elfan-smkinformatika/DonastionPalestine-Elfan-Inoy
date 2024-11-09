f9ce698d725a50bed1242d66cc4307e4c871f47f
elfan-smkinformatika <ackmadelfanpurnama@smkinformatika-crbn.sch.id>
2024-11-09 17:00:50 +0700

    tambah project

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harapan Palestina di Tangan Kita | 2024 Tahun Berbagi</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark-nav py-3 shadow-sm fixed-top">
+        <div class="container-fluid">
+            <img src="" alt="">
+            <a class="navbar-brand fw-bold" href="#">FreeFreePalestine</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#beranda">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#tentang">Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#donasi">Program Donasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#faq">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Start Beranda -->
+    <div id="beranda">
+        <div class="container py-4">
+            <h1 class="tagline-home text-light">Bersama Membangun Harapan untuk Palestina</h1>
+            <h4 class="desc-title-home text-light">Setiap donasi yang Anda berikan adalah secercah harapan bagi warga Palestina yang terus berjuang di tengah kesulitan. Jadilah bagian dari perubahan, dukung hidup, pendidikan, dan kesejahteraan mereka.</h4>
+            <a href="" class="btn p-3 bg-orange text-light mt-4 d-inline-block">Donasi Sekarang</a>
+            <li class="text-light mt-2">Sekecil apa pun donasi Anda, perubahan besar akan terjadi di sana.</li>
+        </div>
+    </div>
+    <!-- End Beranda -->
+
+    <!-- Start Tentang Kami -->
+    <div id="tentang-kami">
+        <div class="container py-5">
+            <h4 class="title-page mb-5">Tentang Kami</h4>
+            <div class="d-flex">
+                <img src="assets/image/about-gaza.png" width="400px" class="me-4 rounded">
+                <div class="content-tentang">
+                    <h3 class="title-tentang">Bersama untuk Palestina: Memberi Harapan di Tengah Krisis</h3>
+                    <p class="paragraf-tentangkami">Kami hadir untuk menjadi jembatan antara kebaikan Anda dan harapan jutaan warga Palestina yang membutuhkan. Di tengah krisis kemanusiaan yang berkepanjangan, kami percaya bahwa setiap bantuan memiliki kekuatan untuk mengubah kehidupan. Kami berkomitmen untuk memastikan setiap donasi yang Anda berikan dapat membawa harapan dan kepedulian nyata ke setiap hati yang membutuhkan.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Tentang Kami -->
+
+    <!-- Start Donasi -->
+    <div id="program-donasi">
+        <div class="container">
+            <h4 class="title-page mb-5">Program Donasi</h4>
+            <!-- Card -->
+             <div class="d-flex justify-content-center">
+                 <div class="card d-flex ">
+                     <img src="assets/image/gaza-pendidikan.png" alt="" width="300px">
+                     <h1 class="title-card my-3">Bantuan Pangan dan Gizi</h1>
+                     <p class="parag-card">Program ini bertujuan untuk menyediakan makanan pokok dan nutrisi bagi keluarga yang terkena dampak konflik. Dengan akses pangan yang terbatas, banyak keluarga di Palestina mengalami krisis kelaparan dan kekurangan gizi, terutama anak-anak dan lansia.</p>
+                 </div>
+                 <div class="card d-flex ">
+                     <img src="assets/image/gaza-pendidikan.png" alt="" width="300px">
+                     <h1 class="title-card my-3">Bantuan Pangan dan Gizi</h1>
+                     <p class="parag-card">Program ini bertujuan untuk menyediakan makanan pokok dan nutrisi bagi keluarga yang terkena dampak konflik. Dengan akses pangan yang terbatas, banyak keluarga di Palestina mengalami krisis kelaparan dan kekurangan gizi, terutama anak-anak dan lansia.</p>
+                 </div>
+                 <div class="card d-flex ">
+                     <img src="assets/image/gaza-pendidikan.png" alt="" width="300px">
+                     <h1 class="title-card my-3">Bantuan Pangan dan Gizi</h1>
+                     <p class="parag-card">Program ini bertujuan untuk menyediakan makanan pokok dan nutrisi bagi keluarga yang terkena dampak konflik. Dengan akses pangan yang terbatas, banyak keluarga di Palestina mengalami krisis kelaparan dan kekurangan gizi, terutama anak-anak dan lansia.</p>
+                 </div>
+                 <div class="card d-flex ">
+                     <img src="assets/image/gaza-pendidikan.png" alt="" width="300px">
+                     <h1 class="title-card my-3">Bantuan Pangan dan Gizi</h1>
+                     <p class="parag-card">Program ini bertujuan untuk menyediakan makanan pokok dan nutrisi bagi keluarga yang terkena dampak konflik. Dengan akses pangan yang terbatas, banyak keluarga di Palestina mengalami krisis kelaparan dan kekurangan gizi, terutama anak-anak dan lansia.</p>
+                 </div>
+             </div>
+
+        </div>
+    </div>    
+    <!-- End Donasi -->
+
+
+    <!-- Script -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
