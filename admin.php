4b7bcaca94f961987bb27fe3d404d31856581059
elfan-smkinformatika <elfan-smkinformatika>
2024-11-14 21:20:27 +0700

    update terbaru, menambahkan style dan form donasi

--- admin.php
+++ admin.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ADMIN | Selamat datang </title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <body>
     
+
+
+
+    <script src="assets/js/bootstrap.bundle.js"></script>
 </body>
 </html>
